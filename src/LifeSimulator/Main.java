1f643d6f679fb157ec3786b429149f0c3ea1d716
MarkThames <markvgilbert@gmail.com>
2018-01-26 23:51:42 -0500

    Update Main.java

--- src/LifeSimulator/Main.java
+++ src/LifeSimulator/Main.java
@@ -6,17 +6,28 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
-public class Main extends JDialog {
+public class Main extends JFrame {
 
+	Island is;
     public Main() {
+    	is=new Island();
         add(new background());
-        setSize(500, 500);
+        setSize(516, 538);
         repaint();
-        setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        
+        //center window
+        GraphicsEnvironment ge=GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice defaultScreen=ge.getDefaultScreenDevice();
+        Rectangle rect=defaultScreen.getDefaultConfiguration().getBounds();
+        int x = ((int)rect.getMaxX() - this.getWidth())/2;
+        int y = 100;
+        setLocation(x, y);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        //show island when pressed and hide
         addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent keyEvent) {
-                Island is = new Island();
                 is.setVisible(true);
                 setVisible(false);
             }
@@ -26,20 +37,23 @@ public class Main extends JDialog {
     private class background extends JPanel {
         private Map m; // used to draw a map
 
+        //faded color
+        private Color fade(Color n){
+        	return new Color((n.getRed()+3*255)/4, (n.getGreen()+3*255)/4, (n.getBlue()+3*255)/4); 
+        }
         public void paintComponent(Graphics G) {
 
             G.setColor(Color.white);
-            //looks fun if you take this line out
             G.fillRect(0, 0, 500, 500);
 
             //determine color for each terrain type
             Color[] mapColors = new Color[6];
-            mapColors[0] = new Color(100, 100, 100);
-            mapColors[1] = new Color(255, 255, 0);
-            mapColors[2] = new Color(160, 40, 40);
-            mapColors[3] = new Color(0, 100, 255);
-            mapColors[4] = new Color(0, 255, 0);
-            mapColors[5] = new Color(0, 150, 50);
+        	mapColors[0]=fade(new Color(100, 100, 100));
+        	mapColors[1]=fade(new Color(255, 255, 0));
+        	mapColors[2]=fade(new Color(160, 40, 40));
+        	mapColors[3]=fade(new Color(0, 100, 255));
+        	mapColors[4]=fade(new Color(0, 255, 0));
+        	mapColors[5]=fade(new Color(0, 150, 50));
 
             //draw map
             for (int i = 0; i < 500; i += 4) {
@@ -48,12 +62,18 @@ public class Main extends JDialog {
                     G.drawRect(i, j, 2, 2);
                 }
             }
+            //add text
             G.setColor(Color.black);
-            G.drawString("Press Any Key to Start", 175, 250);
+            G.setFont(new Font("Britannic Bold", Font.PLAIN, 60));
+            G.drawString("ECOSYSTEM", 100, 120);
+            G.setFont(new Font("Calibri", Font.PLAIN, 18));
+            G.drawString("Press Any Key to Start", 170, 250);
+            G.setFont(new Font("Calibri", Font.PLAIN, 12));
+            G.drawString("Created by: Frank Chen, Roger Fu, Victor Rong", 125, 470);
         }
 
         public background() {
-            m = new Map(800, 800);
+            m = is.getMap();
             setSize(800, 800);
         }
     }
