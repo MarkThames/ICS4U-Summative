04f22f2bb7db8d85776f8129da0ea8c9dcf968b1
MarkThames <markvgilbert@gmail.com>
2018-01-26 23:50:51 -0500

    Update Graph.java

--- src/LifeSimulator/Graph.java
+++ src/LifeSimulator/Graph.java
@@ -10,9 +10,13 @@ import javax.swing.JPanel;
 	class Graph extends JPanel
     {
 		int x, y;
+		//data
 		ArrayList<Integer>[] data;
+		//color of species
 		Color[] c;
+		//two species or not
 		boolean two=false;
+		//constructor
         public Graph (ArrayList<Integer> data1, Color c1)
         {
             this.setPreferredSize (new Dimension (700, 500));
@@ -24,6 +28,7 @@ import javax.swing.JPanel;
             c[0]=c1;
             two=false;
         }
+        //constructor
         public Graph (ArrayList<Integer> data1, ArrayList<Integer> data2, Color c1, Color c2){
         	this.setPreferredSize (new Dimension (700, 500));
             x=120;
@@ -38,6 +43,7 @@ import javax.swing.JPanel;
         }
         public void paintComponent (Graphics g)
         {
+        	//draw graph
         	g.setColor(Color.white);
         	g.fillRect(x, y, 500, 300);
         	g.setColor(Color.black);
@@ -57,7 +63,8 @@ import javax.swing.JPanel;
         	int len=data[0].size()-1;
         	if(len==0)return;
         	g.setColor(c[0]);
-        	//if(len>=14)len=10*(int)(Math.ceil((1.0)*len/10.0));
+        	
+        	//connect data points
         	for(int i=1;i<data[0].size();i++){
         		int x1=x+(500*(i-1))/(len);
         		int y1=y+300-(300*(data[0].get(i-1)-min))/(max-min);
@@ -67,6 +74,7 @@ import javax.swing.JPanel;
         	}
         	g.setColor(Color.black);
         	g.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 9));
+        	//y-axis markings
         	if(max-min<15){
         		for(int i=min;i<=max;i++){
         			g.drawString((""+i), x-25, y+300-(300*(i-min))/(max-min)+4);
@@ -77,6 +85,7 @@ import javax.swing.JPanel;
         			g.drawString((""+(min+i*((max-min)/10))), x-25, y+300-30*i+4);
         		}
         	}
+        	//x-axis markings
         	if(data[0].size()<15){
         		for(int i=0;i<data[0].size();i++){
         			g.drawString((""+i), x-2+(i*500)/(len), y+320);
@@ -88,13 +97,16 @@ import javax.swing.JPanel;
         		}
         	}
         	g.setColor(c[0]);
+        	//write y label
         	String[] word={"P", "o", "p", "u", "l", "a", "t", "i", "o", "n"};
         	g.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 16));
         	for(int i=0;i<10;i++){
         		g.drawString(word[i], x-50, y+80+15*i);
         	}
+        	//write x label
         	g.setColor(Color.black);
-        	g.drawString("Time (Days)", x+200, y+335);
+        	g.drawString("Time (Ticks)", x+200, y+335);
+        	//draw second graph
         	if(two){
         		g.drawLine(x+500, y+300, x+500, y);
         		max=(int)(Integer.MIN_VALUE);
@@ -109,6 +121,7 @@ import javax.swing.JPanel;
             		min--;
             	}
             	g.setColor(c[1]);
+            	//connect points
             	for(int i=1;i<data[0].size();i++){
             		int x1=x+(500*(i-1))/(len);
             		int y1=y+300-(300*(data[1].get(i-1)-min))/(max-min);
@@ -118,6 +131,7 @@ import javax.swing.JPanel;
             	}
             	g.setColor(Color.black);
             	g.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 9));
+            	//y-axis markings
             	if(max-min<15){
             		for(int i=min;i<=max;i++){
             			g.drawString((""+i), x+505, y+300-(300*(i-min))/(max-min)+4);
@@ -130,6 +144,7 @@ import javax.swing.JPanel;
             	}
             	g.setColor(c[1]);
             	g.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 16));
+            	//write y-label
             	for(int i=0;i<10;i++){
             		g.drawString(word[i], x+540, y+80+15*i);
             	}
