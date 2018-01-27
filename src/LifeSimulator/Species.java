9b89a5963a6cfc8f6e5874763263afef8d35abc5
MarkThames <markvgilbert@gmail.com>
2018-01-26 23:52:29 -0500

    Update Species.java

--- src/LifeSimulator/Species.java
+++ src/LifeSimulator/Species.java
@@ -5,15 +5,24 @@ import java.awt.Graphics;
 import java.util.ArrayList;
 
 class Species {
+	//name of species
 	String name;
+	//colour
 	Color c;
+	//how old it gets
 	int adultAge, deathAge;
+	//diet
 	boolean eatPlants;
 	boolean eatAnimals;
-	int id;
+	//id is used to differentiate species
+	//t1 controls size
+	//t2 controls strength
+	int id, t1, t2;
 	int[] size;
+	//strength helps with speed, energy, attacking/defending
 	double strng;
-	public Species(String str, Color col, int ad, int de, boolean pl, boolean an, int i, int minSize, int maxSize, double strength){
+	//constructor
+	public Species(String str, Color col, int ad, int de, boolean pl, boolean an, int i, int t1, int t2){
 		name=str;
 		c=col;
 		adultAge=ad;
@@ -21,18 +30,35 @@ class Species {
 		eatPlants=pl;
 		eatAnimals=an;
 		id=i;
+		this.t1=t1;
+		this.t2=t2;
+		int[][] sizeValues={{2, 3}, {3, 4}, {4, 5}};
+        double[] strengthValues={2.0, 5.0, 12.0};
+        double minSize=sizeValues[t1][0];
+        double maxSize=sizeValues[t1][1];
 		size=new int[2*de+205];
 		for(int j=0;j<2*de+205;j++){
 			size[j]=(int)(minSize+j*(1.0*(maxSize-minSize)/(1.0*de)));
 		}
-		strng=strength;
+		strng=strengthValues[t2];
 	}
-
-	public void update(String str, int ad, int de, boolean pl, boolean an) {
+	//update values
+	public void update(String str, int ad, int de, boolean pl, boolean an, int t1, int t2){
 		name=str;
-		adultAge = ad;
-		deathAge = de;
-		eatPlants = pl;
-		eatAnimals = an;
+		adultAge=ad;
+		deathAge=de;
+		eatPlants=pl;
+		eatAnimals=an;
+		this.t1=t1;
+		this.t2=t2;
+		int[][] sizeValues={{2, 3}, {3, 4}, {7, 9}};
+        double[] strengthValues={2.0, 5.0, 12.0};
+        double minSize=sizeValues[t1][0];
+        double maxSize=sizeValues[t1][1];
+		size=new int[2*de+205];
+		for(int j=0;j<2*de+205;j++){
+			size[j]=(int)(minSize+j*(1.0*(maxSize-minSize)/(1.0*de)));
+		}
+		strng=strengthValues[t2];
 	}
 }
